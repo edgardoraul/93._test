9cdddcc5d86768f3b90c36b1b81ba0959750ea0e
Edgard_Roul <edgardo_raul@hotmail.com>
2024-10-04 12:22:13 -0300

    Reemplazo de la estructura de la plantilla para hacer mejor testing

---
+++ wp-content/themes/twentytwentytwo/template-parts/footer/site-info.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<?php
+	if ( function_exists( 'the_privacy_policy_link' ) ) {
+		the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+	}
+	
+	custom_theme_display_social_links();
+	?>
+</div><!-- .site-info -->
